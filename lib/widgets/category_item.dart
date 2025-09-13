2abdf26fc7ca8ce55b2135a5595ae0654f6ab0e9
umeh-promise <umehpromise1000@gmail.com>
2025-09-13 16:06:47 +0100

    feat: add category functionality

---
+++ lib/widgets/category_item.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/models/category.dart';
+import 'package:meals_app/models/meal.dart';
+
+class CategoryItem extends StatelessWidget {
+  final Category category;
+  final void Function(BuildContext, Category, List<Meal>) onSelectCategory;
+  const CategoryItem({
+    super.key,
+    required this.category,
+    required this.onSelectCategory,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () => onSelectCategory(context, category, []),
+      splashColor: Theme.of(context).primaryColor,
+      child: Container(
+        padding: EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(4),
+          gradient: LinearGradient(
+            colors: [
+              category.color.withValues(alpha: 0.55),
+              category.color.withValues(alpha: 0.9),
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: Text(
+          category.title,
+          style: Theme.of(context).textTheme.titleLarge!.copyWith(
+            color: Theme.of(context).colorScheme.onSurface,
+          ),
+        ),
+      ),
+    );
+  }
+}
